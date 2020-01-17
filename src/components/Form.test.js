66f7c32b86cea40413d31ba1dd065710465c21e6
JinSeal <1183958@gmail.com>
2020-01-17 21:56:50 +1100

    Finished form component TDD

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,16 +1,74 @@
 import React from "react";
-import Enzyme, { shadow } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import Form from "./Form";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-it("renders without crashing", () => {});
+/**
+ * Factory function to create a ShallowWrapper for form component.
+ * @function setup
+ * @params {object} props - Component props specifit to this setup
+ * @params {any} state - Initial state
+ * @return {ShallowWrapper}
+ */
 
-it("renders form", () => {});
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<Form {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
 
-it("renders input box", () => {});
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for search.
+ * @return {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
 
-it("renders dropdown buttons", () => {});
+it("renders form", () => {
+  const wrapper = setup();
+  const formComponent = findByTestAttr(wrapper, "component-form");
+  expect(formComponent.length).toBe(1);
+});
 
-it("changes value state on clicking buttons");
+it("renders input box", () => {
+  const wrapper = setup();
+  const inputComponent = findByTestAttr(wrapper, "component-input");
+  expect(inputComponent.length).toBe(1);
+});
+
+it("initial state as `Annually`", () => {
+  const wrapper = setup();
+  const initialBottonValueState = wrapper.state("buttonValue");
+  expect(initialBottonValueState).toBe("Annually");
+});
+
+it("renders dropdown button group", () => {
+  const wrapper = setup();
+  const buttonValue = findByTestAttr(wrapper, "component-button");
+  expect(buttonValue.length).toBe(1);
+});
+
+it("renders dropdown buttons", () => {
+  const wrapper = setup();
+  const buttonValue = findByTestAttr(wrapper, "component-button");
+  expect(buttonValue.text()).toBe("Annually");
+});
+
+it("changes value state on clicking buttons", () => {
+  const wrapper = setup();
+
+  const button = wrapper.find("[value='Weekly']");
+
+  const mockedEvent = { target: { value: "Weekly" } };
+  button.simulate("click", mockedEvent);
+
+  const buttonValue = findByTestAttr(wrapper, "component-button");
+
+  expect(wrapper.state("buttonValue")).toBe("Weekly");
+  expect(buttonValue.text()).toBe("Weekly");
+});
